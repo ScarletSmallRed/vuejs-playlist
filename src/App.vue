14615b70df48537768daf837c5244ccecae4b411
沈韶泓 <shenshaohong@shenshaohongdeMacBook-Pro-2.local>
2018-08-14 21:25:32 +0800

    input binding, creating a blog part 1

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,18 @@
 <template>
   <div>
-    <app-header
-      :title="title"
-      @changeTitle="updateTitle"></app-header>
-    <app-ninjas
-      :ninjas="ninjas"></app-ninjas>
-    <app-footer :title="title"></app-footer>
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Ninjas from "./components/Ninjas";
+import addBlog from "./components/addBlog"
 export default {
   components: {
-    "app-header": Header,
-    "app-footer": Footer,
-    "app-ninjas": Ninjas
+    "add-blog": addBlog
   },
   data() {
     return {
-      title: "Your first Vue file, wooo!",
-      ninjas: [
-        { name: "Ryu", speciality: "Vue Components", show: false },
-        { name: "Crystal", speciality: "HTML Wizardry", show: false },
-        { name: "Hitoshi", speciality: "Click Events", show: false },
-        { name: "Tango", speciality: "Conditionals", show: false },
-        { name: "Kami", speciality: "Webpack", show: false },
-        { name: "Yoshi", speciality: "Data Diggin", show: false }
-      ]
+
     };
   },
   methods: {
