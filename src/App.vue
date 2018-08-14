75a06ae1e3fb5ca5a35716d5f2fd573098a8ecf9
沈韶泓 <shenshaohong@shenshaohongdeMacBook-Pro-2.local>
2018-08-14 11:19:50 +0800

    props example

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <app-header></app-header>
-    <app-ninjas></app-ninjas>
+    <app-ninjas :ninjas="ninjas"></app-ninjas>
     <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import Ninjas from "./components/Ninjas"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Ninjas from "./components/Ninjas";
 export default {
   components: {
     "app-header": Header,
@@ -18,7 +18,15 @@ export default {
   },
   data() {
     return {
-      title: "Your first Vue file, wooo!"
+      title: "Your first Vue file, wooo!",
+      ninjas: [
+        { name: "Ryu", speciality: "Vue Components", show: false },
+        { name: "Crystal", speciality: "HTML Wizardry", show: false },
+        { name: "Hitoshi", speciality: "Click Events", show: false },
+        { name: "Tango", speciality: "Conditionals", show: false },
+        { name: "Kami", speciality: "Webpack", show: false },
+        { name: "Yoshi", speciality: "Data Diggin", show: false }
+      ]
     };
   },
   methods: {
