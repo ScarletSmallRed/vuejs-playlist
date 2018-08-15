05b4b712cad717bf32e5eb0804f3c4ebc5fb59fe
沈韶泓 <shenshaohong@shenshaohongdeMacBook-Pro-2.local>
2018-08-15 16:33:14 +0800

    select box

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -17,6 +17,12 @@
                 <label>Cheese</label>
                 <input type="checkbox" value="cheese" v-model="blog.categories" />
             </div>
+            <label>Author:</label>
+            <select v-model="blog.author">
+                <option
+                  v-for="author in authors"
+                  :key="author">{{ author }}</option>
+            </select>
         </form>
         <div id="preview">
             <h3>Preview blog</h3>
@@ -27,6 +33,7 @@
             <ul>
                 <li v-for="category in blog.categories" :key="category">{{ category }}</li>
             </ul>
+            <p>Author: {{ blog.author }}</p>
         </div>
     </div>
 </template>
@@ -34,50 +41,51 @@
 <script>
 // Imports
 export default {
-    data () {
-        return {
-            blog: {
-                title: '',
-                content: '',
-                categories: []
-            }
-        }
-    },
-    methods: {
-    }
-}
+  data() {
+    return {
+      blog: {
+        title: "",
+        content: "",
+        categories: []
+      },
+      authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator']
+    };
+  },
+  methods: {}
+};
 </script>
 
 <style>
-#add-blog *{
-    box-sizing: border-box;
+#add-blog * {
+  box-sizing: border-box;
 }
-#add-blog{
-    margin: 20px auto;
-    max-width: 500px;
+#add-blog {
+  margin: 20px auto;
+  max-width: 500px;
 }
-label{
-    display: block;
-    margin: 20px 0 10px;
+label {
+  display: block;
+  margin: 20px 0 10px;
 }
-input[type="text"], textarea{
-    display: block;
-    width: 100%;
-    padding: 8px;
+input[type="text"],
+textarea {
+  display: block;
+  width: 100%;
+  padding: 8px;
 }
-#preview{
-    padding: 10px 20px;
-    border: 1px dotted #ccc;
-    margin: 30px 0;
+#preview {
+  padding: 10px 20px;
+  border: 1px dotted #ccc;
+  margin: 30px 0;
 }
-h3{
-    margin-top: 10px;
+h3 {
+  margin-top: 10px;
 }
-#checkboxes input{
-    display: inline-block;
-    margin-right: 10px;
+#checkboxes input {
+  display: inline-block;
+  margin-right: 10px;
 }
-#checkboxes label{
-    display: inline-block;
+#checkboxes label {
+  display: inline-block;
 }
 </style>
