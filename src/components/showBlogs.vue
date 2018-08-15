f1c740262547e035c1110d4b26df2e9f129dfd63
沈韶泓 <shenshaohong@shenshaohongdeMacBook-Pro-2.local>
2018-08-15 20:05:11 +0800

    filters

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,8 +1,9 @@
 <template>
     <div id="show-blogs">
         <h1>All Blog Articles</h1>
-        <div v-for="blog in blogs" class="single-blog" :key="blog.title">
-            <h2>{{ blog.title }}</h2>
+        <input type="text" v-model="search" placeholder="search blogs" />
+        <div v-for="blog in filteredBlogs" class="single-blog" :key="blog.title">
+            <h2 v-rainbow>{{ blog.title | toUpperCase }}</h2>
             <article>{{ blog.body }}</article>
         </div>
     </div>
@@ -12,7 +13,8 @@
 export default {
   data() {
     return {
-      blogs: []
+      blogs: [],
+      search: ""
     };
   },
   methods: {},
@@ -21,10 +23,35 @@ export default {
       method: "get",
       url: "/posts",
       baseURL: "http://jsonplaceholder.typicode.com/"
-    })
-      .then(res => {
-        this.blogs = res.data.slice(0, 10)
-      })
+    }).then(res => {
+      this.blogs = res.data.slice(0, 10);
+    });
+  },
+  filters: {
+    /*'to-uppercase': function(value){
+            return value.toUpperCase();
+        }*/
+    toUpperCase(value) {
+      return value.toUpperCase();
+    }
+  },
+  directives: {
+    rainbow: {
+      bind(el, binding, vnode) {
+        el.style.color =
+          "#" +
+          Math.random()
+            .toString(16)
+            .slice(2, 8);
+      }
+    }
+  },
+  computed: {
+    filteredBlogs: function() {
+      return this.blogs.filter(blog => {
+        return blog.title.match(this.search);
+      });
+    }
   }
 };
 </script>
